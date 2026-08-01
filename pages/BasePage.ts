76e18e900876f0118fd66d2efa648aa280ff2456
Potturi Naga Phani Kumar <phanison@Potturis-MacBook-Air.local>
2026-08-01 17:46:21 +0530

    Add screenshots logic

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -1,13 +1,20 @@
 import { Page } from "@playwright/test";
 
 export class BasePage {
-    constructor(protected page: Page) { }
+  constructor(protected page: Page) {}
 
-    async navigate(url: string) {
-        await this.page.goto(url);
-    }
+  async navigate(url: string) {
+    await this.page.goto(url);
+  }
 
-    async getTitle(): Promise<string> {
-        return await this.page.title(); 
-    }
+  async getTitle(): Promise<string> {
+    return await this.page.title();
+  }
+
+  async takeScreenshot(name: string): Promise<void> {
+    await this.page.screenshot({
+      path: `screenshots/${name}.png`,
+      fullPage: true,
+    });
+  }
 }
